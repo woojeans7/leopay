cf0dd4a9b458b6f0ae387b523a785b0af8dc3a08
woojeans7 <thddnwls71@naver.com>
2026-05-27 22:46:52 +0900

    fix: booking-api EntityScan, JpaRepositories 패키지 스캔 추가

--- booking-api/src/main/kotlin/com/leopay/bookingapi/BookingApiApplication.kt
+++ booking-api/src/main/kotlin/com/leopay/bookingapi/BookingApiApplication.kt
@@ -1,11 +1,15 @@
 package com.leopay.bookingapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
+@EntityScan(basePackages = ["com.leopay.storage.entity"])
+@EnableJpaRepositories(basePackages = ["com.leopay.storage.repository"])
 class BookingApiApplication
 
 fun main(args: Array<String>) {
