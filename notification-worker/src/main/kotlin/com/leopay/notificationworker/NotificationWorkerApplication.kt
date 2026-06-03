f66e437392885ee86ea0dbce4153ef321e3a2f3d
woojeans7 <thddnwls71@naver.com>
2026-06-03 23:59:19 +0900

    fix: notification-worker 기동 오류 수정 (JPA Auditing, ObjectMapper, DB 접속)

--- notification-worker/src/main/kotlin/com/leopay/notificationworker/NotificationWorkerApplication.kt
+++ notification-worker/src/main/kotlin/com/leopay/notificationworker/NotificationWorkerApplication.kt
@@ -4,8 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 @EntityScan(basePackages = ["com.leopay.storage.entity"])
 @EnableJpaRepositories(basePackages = ["com.leopay.storage.repository"])
 class NotificationWorkerApplication
